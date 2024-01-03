668202b264291c7b93f77c0e9789c95180b3d2b6
DIYEMA <zu21512@bristol.ac.uk>
2024-01-03 11:13:23 +0000

    old_version

--- src/index.js
+++ src/index.js
@@ -22,8 +22,11 @@ import {
   saveSolidDatasetAt,
   setThing,
   saveFileInContainer,
+  isRawData,
+  getContentType,
   overwriteFile,
-  getSourceUrl
+  getSourceUrl,
+  getFile
 } from "@inrupt/solid-client";
 
 const ChildPodLink = "https://test.pod.ewada.ox.ac.uk/SydChaOx/Location/";
@@ -44,6 +47,9 @@ const locradValues = [25, 50, 75, 100, 150, 200, 300, 400, 500, 600];
 let currentData = [];
 let smartwatchData = [];
 var map = [];
+var pmap = [];
+var locdet = 1;
+var disdet = 1;
 
 // 1a. Start Login Process. Call login() function.
 function loginToSelectedIdP() {
@@ -73,41 +79,39 @@ async function handleRedirectAfterLogin() {
     waitpage.style.display = "none";
     if (session.info.webId === "https://test.pod.ewada.ox.ac.uk/SydChaOx/profile/card#me") {
       page2.style.display = "Flex";
-      viewData();
+      childViewData();
 
     } else if (session.info.webId === "https://sydcha.solidcommunity.net/profile/card#me") {
       page3.style.display = "Flex";
+      parentViewData();
+
     } else {
       console.log(document.getElementById("myWebID").value);
     }
   }
 }
 
-function makeMap(){
+function makeMap() {
   map = L.map('map').setView([51.4774, 0.0007], 13);
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
 }
 
 
 handleRedirectAfterLogin();
 
-async function viewData() {
+async function childViewData() {
   try {
-    
-
-    const webID = document.getElementById("myWebID").value;
-
-    const mypods = await getPodUrlAll(webID, { fetch: fetch });
     // const auth = solid.auth;
     const auth = getDefaultSession();
     const fc = new SolidFileClient(auth);
     const folder = await fc.readFolder(ChildPodLink);
-    smartwatchData = [];
 
+    const fileBlob = await getFile(`${SharedPodLink}SmartwatchData`, { fetch: fetch });
+    // console.log(fileBlob)
     // Use Promise.all to await all the asynchronous file reading operations
     const promises = folder.files.map(file => fc.readFile(file.url));
     const data = await Promise.all(promises);
@@ -119,13 +123,80 @@ async function viewData() {
     detailedData = coordinateStrings.map(coordStr => {
       const [lat, lon] = coordStr.replace(/[\[\]]/g, '').split(',').map(Number);
       return [lat, lon];
-    })
+    });
 
     genData();
     makeMap();
     genShapes();
 
+  } catch (error) {
+    console.log(error);
+    labelCreateStatus.textContent = "Error" + error;
+    labelCreateStatus.setAttribute("role", "alert");
+  }
 
+}
+async function readFileFromPod(fileURL) {
+  try {
+    const file = await getFile(
+      fileURL,               // File in Pod to Read
+      { fetch: fetch }       // fetch from authenticated session
+    );
+
+    console.log(`Fetched a ${getContentType(file)} file from ${getSourceUrl(file)}.`);
+
+    return file;
+
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+
+async function parentViewData() {
+  try {
+    
+    const auth = getDefaultSession();
+    const fc = new SolidFileClient(auth);
+    const blob = await readFileFromPod(`${SharedPodLink}SmartwatchData`);
+
+    // Extract the text content from the Blob
+    const jsonText = await blob.text();
+
+    // Parse the JSON text into an object
+    const jsonObject = JSON.parse(jsonText);
+
+    currentData = jsonObject.currentData;
+    locdet = jsonObject.locationSlider;
+    disdet = jsonObject.timeSlider;
+    console.log(jsonObject.currentData);
+    console.log(currentData);
+
+    pmap = L.map('pmap').setView([51.4774, 0.0007], 13);
+    console.log(pmap)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(pmap);
+    var pmarkerLayer = L.layerGroup();
+    pmarkerLayer.clearLayers();
+
+    if (currentData && currentData.length > 0) {
+      for (const coordinates of currentData) {
+        const [latitude, longitude] = coordinates; // Destructure the array into latitude and longitude
+        L.circle([latitude, longitude], {
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.1,
+          radius: locradValues[locdet - 1]
+        }).addTo(pmarkerLayer);
+
+      }
+      pmarkerLayer.addTo(pmap);
+    } else {
+      console.log("current data is null or empty")
+    }
   } catch (error) {
     console.log(error);
     labelCreateStatus.textContent = "Error" + error;
@@ -135,21 +206,7 @@ async function viewData() {
 }
 
 
-// Upload file into the targetContainer.
-async function placeFileInContainer(file, targetContainerURL) {
-  try {
-    const savedFile = await saveFileInContainer(
-      targetContainerURL,           // Container URL
-      file,                         // File 
-      { slug: file.name, contentType: file.type, fetch: fetch }
-    );
-    console.log(`File saved at ${getSourceUrl(savedFile)}`);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-async function overwriteSharedArray(){
+async function overwriteSharedArray() {
   try {
     const dataToSave = {
       currentData: currentData,
@@ -158,16 +215,16 @@ async function overwriteSharedArray(){
     };
     // Create a Blob from the text content
     const dataToSaveString = JSON.stringify(dataToSave);
-    const blob = new Blob([dataToSaveString], { type: 'text/csv' });
+    const blob = new Blob([dataToSaveString], { type: 'text/plain' });
 
     // Create a File object with the Blob and file name
-    const file = new File([blob], "SmartwatchData", { type: 'text/csv' });
+    const file = new File([blob], "SmartwatchData", { type: 'text/plain' });
 
     // Call the existing function to save the file
     const savedFile = await overwriteFile(
       `${SharedPodLink}${file.name}`,           // Container URL
       file,                         // File
-      {contentType: file.type, fetch: fetch }
+      { contentType: file.type, fetch: fetch }
     );
 
     console.log(`File saved at ${getSourceUrl(savedFile)}`);
@@ -204,10 +261,7 @@ var markerLayer = L.layerGroup();
 
 function genShapes() {
 
-  const locdet = parseInt(locationDetailSlider.value);
-  const disdet = parseInt(displayDetailSlider.value);
 
-  console.log(locdet);
   markerLayer.clearLayers();
   if (currentData && currentData.length > 0) {
     for (const coordinates of currentData) {
@@ -228,22 +282,20 @@ function genShapes() {
 }
 
 function genData() {
-  const timedet = displayDetailSlider.value;
-  const locdet = locationDetailSlider.value
-  const locdetIncrement = 0.0002 * locdet;
-  currentData = detailedData.filter((_, index) => index % timedet === 0);
+
+  const locdetIncrement = 0.0002 * locationDetailSlider.value;
+  currentData = detailedData.filter((_, index) => index % displayDetailSlider.value === 0);
   currentData = currentData.map(point => {
     const lat = point[0] + (Math.random() * 2 - 1) * locdetIncrement;
     const lon = point[1] + (Math.random() * 2 - 1) * locdetIncrement;
     return [lat, lon];
   });
-  
+
 }
 
 
 locationDetailSlider.addEventListener("input", function () {
-  const locationDetailValue = parseInt(locationDetailSlider.value);
-  const displayDetailValue = parseInt(displayDetailSlider.value);
+  locdet = parseInt(locationDetailSlider.value);
 
   genData();
   genShapes()
@@ -251,8 +303,7 @@ locationDetailSlider.addEventListener("input", function () {
 });
 
 displayDetailSlider.addEventListener("input", function () {
-  const displayDetailValue = parseInt(displayDetailSlider.value);
-  const locationDetailValue = parseInt(locationDetailSlider.value);
+  disdet = parseInt(displayDetailSlider.value);
 
   genData();
   genShapes();
